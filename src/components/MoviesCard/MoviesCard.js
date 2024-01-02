f2e9efdcc5790a5dc925effc058ae81eb12c6ba8
Olga Liubimova <lov800@yandex.ru>
2024-01-02 18:57:14 +0300

    Сверстать карточку фильма

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,30 @@
 import "./MoviesCard.css";
+import React from "react";
 
-function MoviesCard() {
-  return <li className="movies-card"></li>;
+function MoviesCard({ movieImage }) {
+  const [isLiked, setIsLiked] = React.useState(false);
+
+  function toggleLike() {
+    setIsLiked(!isLiked);
+  }
+
+  return (
+    <li className="movies-card">
+      <img
+        className="movies-card__image"
+        alt="Тестовое фото"
+        src={movieImage}
+      ></img>
+      <h2 className="movies-card__title">33 слова о дизайне</h2>
+      <button
+        className={`movies-card__like-button ${
+          isLiked ? "movies-card__like-button_active" : ""
+        }`}
+        onClick={toggleLike}
+      ></button>
+      <p className="movies-card__length">1ч42м</p>
+    </li>
+  );
 }
 
 export default MoviesCard;
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,26 +1,30 @@
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import movieImageOne from "../../images/movies/movie01.png";
+import movieImageTwo from "../../images/movies/movie02.png";
+import movieImageThree from "../../images/movies/movie03.png";
+import movieImageFour from "../../images/movies/movie04.png";
 
 function MoviesCardList() {
   return (
     <section aria-label="Список фильмов">
       <ul className="movies-cardlist">
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
+        <MoviesCard movieImage={movieImageOne} />
+        <MoviesCard movieImage={movieImageTwo} />
+        <MoviesCard movieImage={movieImageThree} />
+        <MoviesCard movieImage={movieImageFour} />
+        <MoviesCard movieImage={movieImageOne} />
+        <MoviesCard movieImage={movieImageTwo} />
+        <MoviesCard movieImage={movieImageThree} />
+        <MoviesCard movieImage={movieImageFour} />
+        <MoviesCard movieImage={movieImageOne} />
+        <MoviesCard movieImage={movieImageTwo} />
+        <MoviesCard movieImage={movieImageThree} />
+        <MoviesCard movieImage={movieImageFour} />
+        <MoviesCard movieImage={movieImageOne} />
+        <MoviesCard movieImage={movieImageTwo} />
+        <MoviesCard movieImage={movieImageThree} />
+        <MoviesCard movieImage={movieImageFour} />
       </ul>
     </section>
   );
